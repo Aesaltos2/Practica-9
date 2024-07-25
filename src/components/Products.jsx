fde7d74808eb836680f3ac84388ab602aab2ef79
Adrian Saltos <Asaltos>
2024-07-25 12:11:16 -0500

    Upload Practica 9

---
+++ src/components/Products.jsx
@@ -0,0 +1,48 @@
+import './Products.css'
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons.jsx'
+import { useCart } from '../hooks/useCart.js'
+
+export function Products({ products }) {
+  const { addToCart, removeFromCart, cart } = useCart()
+
+  const checkProductInCart = product => {
+    return cart.some(item => item.id === product.id)
+  }
+
+  return (
+    <main className='products'>
+      <ul>
+        {products.slice(0, 1000).map(product => {
+          const isProductInCart = checkProductInCart(product)
+
+          return (
+            <li key={product.id}>
+              <img
+                src={`https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/200/300`}
+                alt={product.title}
+              />
+              <div>
+                <strong>{product.title}</strong> - ${product.price}
+              </div>
+              <div>
+                <button
+                  style={{ backgroundColor: isProductInCart ? 'red' : '#09f' }} onClick={() => {
+                    isProductInCart
+                      ? removeFromCart(product)
+                      : addToCart(product)
+                  }}
+                >
+                  {
+                    isProductInCart
+                      ? <RemoveFromCartIcon />
+                      : <AddToCartIcon />
+                  }
+                </button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
+}
